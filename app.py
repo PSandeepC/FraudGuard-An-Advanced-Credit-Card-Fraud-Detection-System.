54179ff9500fd49a1e47f43becc17c48bd77b149
PSandeepC <137529886+PSandeepC@users.noreply.github.com>
2024-01-20 20:55:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,6 +57,10 @@ if nav_choice == "Home":
 elif nav_choice == "Oversampled Data":
     st.header("Oversampled Data Input and Prediction")
     st.write("To address class imbalance in our credit card fraud detection project, we employed oversampling. This technique involves creating additional instances of the minority class (fraudulent transactions) to achieve a more balanced dataset. Oversampling enhances the model's ability to detect instances of fraud, contributing to a more robust and reliable fraud detection system.")
+    st.write("**Example for Fraudulent Transaction:**")
+    st.text("[27, 15, 24, 38, 21, 48, 1, 25, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 83, 74, 56, 43, 29]")
+     st.write("**Example for Normal Transaction:**")
+    st.text("[1, 1, 1, 1, 1, 1, 1, 27, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]")
     # Option to manually enter input for oversampled data
     st.subheader("Enter Oversampled Data Input:")
     feature_values_resampled = [st.slider(f"Feature {i}", min_value=0, max_value=100, value=example_input_resampled[i-1]) for i in range(1, 30)]
