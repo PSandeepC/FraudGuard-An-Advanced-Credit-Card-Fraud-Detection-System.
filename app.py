83e9e8b1c1584eed05bae838ec7b8a25c40ccc87
PSandeepC <137529886+PSandeepC@users.noreply.github.com>
2024-01-20 13:16:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,31 +5,10 @@ import numpy as np
 
 # Load your models
 model_log = pickle.load(open('logistic_regression_model.pkl', 'rb'))
-model_rf = pickle.load(open('random_forest_model.pkl', 'rb'))
+
 model_log_resampled = pickle.load(open('log_resampled_model.pkl', 'rb'))
-model_rf_resampled = pickle.load(open('rf_resampled_model.pkl', 'rb'))
 
-# Function to fix the dtype of the node array for Random Forest model
-def fix_rf_dtype(tree_):
-    expected_dtype_rf = [
-        ('left_child', '<i8'),
-        ('right_child', '<i8'),
-        ('feature', '<i8'),
-        ('threshold', '<f8'),
-        ('impurity', '<f8'),
-        ('n_node_samples', '<i8'),
-        ('weighted_n_node_samples', '<f8'),
-        ('missing_go_to_left', 'u1')
-    ]
 
-    # Check if the dtype needs to be fixed
-    if tree_.dtype != np.dtype(expected_dtype_rf):
-        tree_ = np.array(tree_, dtype=expected_dtype_rf)
-    return tree_
-
-# Fix the dtype for each estimator in the Random Forest model
-for estimator in model_rf.estimators_:
-    estimator.tree_.__getstate__()['nodes'] = fix_rf_dtype(estimator.tree_.__getstate__()['nodes'])
 
 # Example input for user guidance
 example_input_resampled = [1, 1, 1, 1, 1, 1, 1, 25, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
@@ -53,9 +32,9 @@ if nav_choice == "Home":
         # Use your models to make predictions based on user input
         input_data = [feature_values]
         prediction_log = model_log.predict(input_data)[0]
-        prediction_rf = model_rf.predict(input_data)[0]
+       
 
-        prediction_result = "Normal Transaction" if prediction_log == 0 and prediction_rf == 0 else "Fraudulent Transaction"
+        prediction_result = "Normal Transaction" if prediction_log == 0  else "Fraudulent Transaction"
         st.success(f"Prediction: {prediction_result}")
 
 # Section 2: Display Oversampled Data
@@ -71,9 +50,8 @@ elif nav_choice == "Oversampled Data":
         # Use your models to make predictions based on oversampled data input
         input_data_resampled = [feature_values_resampled]
         prediction_log_resampled = model_log_resampled.predict(input_data_resampled)[0]
-        prediction_rf_resampled = model_rf_resampled.predict(input_data_resampled)[0]
-
-        prediction_result_resampled = "Normal Transaction" if prediction_log_resampled == 0 and prediction_rf_resampled == 0 else "Fraudulent Transaction"
+       
+        prediction_result_resampled = "Normal Transaction" if prediction_log_resampled  else "Fraudulent Transaction"
         st.success(f"Prediction (Oversampled Data): {prediction_result_resampled}")
 
 # Section 3: Model Scores and Graphs
